d1253061ab3a7fc2d0ce500c01a555b7f1e2ccef
lumenwibowo267 <lumenwibowo@gmail.com>
2026-05-18 03:17:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
+const express = require("express");
+const qrcode = require("qrcode");
 const {
     default: makeWASocket,
     useMultiFileAuthState,
     fetchLatestBaileysVersion
 } = require("@whiskeysockets/baileys");
 
-const qrcode = require("qrcode-terminal");
+const app = express();
+let latestQR = null;
+
+app.get("/", async (req, res) => {
+    if (!latestQR) {
+        return res.send("<h2>QR belum muncul, tunggu bot jalan...</h2>");
+    }
+
+    const qrImage = await qrcode.toDataURL(latestQR);
+
+    res.send(`
+        <h2>Scan QR WhatsApp</h2>
+        <img src="${qrImage}" />
+    `);
+});
+
+app.listen(3000, () => {
+    console.log("🌐 QR Web running: http://localhost:3000");
+});
 
 async function startBot() {
     const { state, saveCreds } = await useMultiFileAuthState("./auth");
@@ -16,26 +36,22 @@ async function startBot() {
         printQRInTerminal: false
     });
 
-   sock.ev.on("connection.update", (update) => {
-    const { connection, qr } = update;
+    sock.ev.on("connection.update", (update) => {
+        const { connection, qr } = update;
 
-    if (qr) {
-        console.clear();
-        console.log("=== SCAN QR DI BAWAH ===\n");
+        if (qr) {
+            latestQR = qr;
+            console.log("QR updated (open http://localhost:3000)");
+        }
 
-        qrcode.generate(qr, {
-            small: false
-        });
-    }
+        if (connection === "open") {
+            console.log("✅ BOT CONNECTED");
+        }
 
-    if (connection === "open") {
-        console.log("✅ BOT CONNECTED");
-    }
-
-    if (connection === "close") {
-        console.log("❌ CONNECTION CLOSED");
-    }
-});
+        if (connection === "close") {
+            console.log("❌ CONNECTION CLOSED");
+        }
+    });
 
     sock.ev.on("creds.update", saveCreds);
 }
